440a374835f1386cdee626ecd8147932fa487671
Brayden Hill <Hillbgh@gmail.com>
2022-04-16 22:12:55 -0600

    rendering, but flashes after like 10 seconds

--- rules.mk
+++ rules.mk
@@ -1,6 +1,20 @@
-MOUSEKEY_ENABLE = yes    # Mouse keys
-RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
+MOUSEKEY_ENABLE = no    # Mouse keys
+RGBLIGHT_ENABLE = yes        # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
 LTO_ENABLE      = yes
 WPM_ENABLE = yes
+
+ifeq ($(strip $(OLED_ENABLE)), yes)
+    #... your code here...
+    ifdef OCEAN_DREAM_ENABLE
+        ifeq ($(strip $(OCEAN_DREAM_ENABLE)), yes)
+            SRC += ocean_dream.c
+            OPT_DEFS += -DOCEAN_DREAM_ENABLE
+        endif
+    endif
+    ifndef OCEAN_DREAM_ENABLE
+        SRC += ocean_dream.c
+        OPT_DEFS += -DOCEAN_DREAM_ENABLE
+    endif
+endif
